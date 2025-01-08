1220fabe7c70b0090621aa4473ec4e083289fed4
gianna <gia.mar.yae@gmail.com>
2025-01-08 09:52:35 +0100

    ex 4.1

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -5,18 +5,18 @@
 
 
 trait IsEven {
-    fn is_even(self) -> bool;
+    fn is_even(&self) -> bool;
 }
 
 impl IsEven for u32 {
-    fn is_even(self) -> bool {
+    fn is_even(&self) -> bool {
         (self % 2) == 0
     }
 }
 
 
 impl IsEven for i32 {
-    fn is_even(self) -> bool {
+    fn is_even(&self) -> bool {
         (self % 2) == 0
     }
 }
